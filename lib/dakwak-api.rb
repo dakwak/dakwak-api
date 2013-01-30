6c3c27ed19fec77d5a7b3eb1ae900e5bb6790839
nour <nour.fwh@gmail.com>
2013-01-30 15:23:37 +0200

    fix methods response

--- lib/dakwak-api.rb
+++ lib/dakwak-api.rb
@@ -9,31 +9,45 @@ module Dakwak
       base_uri 'http://localhost:3000/api'
 
       def initialize(apikey)
+        raise ArgumentError, "apikey must be a non-empty string" unless is_non_empty_string(apikey)
         @apikey = apikey
       end
 
       def search(phrase, lang)
+        raise ArgumentError, "phrase must be a non-empty string" unless is_non_empty_string(phrase)
+        raise ArgumentError, "lang must be a non-empty string" unless is_non_empty_string(lang)
         url = "/search.json?apikey=#{@apikey}&phrase=#{phrase}&lang=#{lang}"
-        return self.class.post(url)
+        return self.class.post(url).body
       end
 
       def get_translation(phrase, lang)
+        raise ArgumentError, "phrase must be a non-empty string" unless is_non_empty_string(phrase)
+        raise ArgumentError, "lang must be a non-empty string" unless is_non_empty_string(lang)
         url = "/get_translation.json?apikey=#{@apikey}&phrase=#{phrase}&lang=#{lang}"
-        return self.class.post(url)
+        return self.class.post(url).body
       end
 
       def translate(phrases, lang, callback_url="")
+        raise ArgumentError, "phrases must be a non-empty array" unless is_non_empty_array(phrases)
+        raise ArgumentError, "lang must be a non-empty string" unless is_non_empty_string(lang)
         url = "/translate.json?apikey=#{@apikey}&lang=#{lang}&callback_url=#{callback_url}"
-        phrases = [phrases] unless phrases.is_a?(Array)
         phrases.each { |phrase| url << "&phrases[]=#{phrase}" }
-        return self.class.post(url)
+        return self.class.post(url).body
       end
 
       def index_pages(pages)
-        url = "/index_page.json?apikey=#{@apikey}"
-        pages = [pages] unless pages.is_a?(Array) 
+        raise ArgumentError, "pages must be a non-empty array" unless is_non_empty_array(pages)
+        url = "/index_pages.json?apikey=#{@apikey}"
         pages.each { |page| url << "&pages[]=#{page}" }
-        return self.class.post(url)
+        return self.class.post(url).body
+      end
+
+      private
+      def is_non_empty_string(arg)
+        arg.is_a?(String) && !arg.empty? 
+      end
+      def is_non_empty_array(arg)
+        arg.is_a?(Array) && !arg.empty? 
       end
     end
   end
